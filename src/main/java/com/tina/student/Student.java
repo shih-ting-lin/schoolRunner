6af954dfd17b643596c7fef8f6da69ad2f3565ac
shih-ting-lin <107251983+shih-ting-lin@users.noreply.github.com>
2022-09-15 16:42:18 +0800

    Update Student.java

--- src/main/java/com/tina/student/Student.java
+++ src/main/java/com/tina/student/Student.java
@@ -1,2 +1,45 @@
-package com.tina.student;public class Student {
+package com.tina.student;
+
+public class Student {
+    String id;
+    String name;
+    int english;
+    int math;
+    static int pass = 60;
+
+    public Student(String id,String name,int english,int math){
+        this.id = id;
+        this.name = name;
+        this.english = english;
+        this.math = math;
+    }
+    public void print(){
+        int average = getAverage();
+        System.out.print(name+"\t"+id+"\t"+english+"\t"+math+
+                "\t"+((getAverage()>=pass)? "pass": "failed"));
+        char grading = 'A';
+        switch (average/10){
+            case 10:
+            case 9:
+                grading = 'A';
+                break;
+            case 8:
+                grading = 'B';
+                break;
+            case 7:
+                grading = 'C';
+                break;
+            case 6:
+                grading = 'D';
+                break;
+            default:
+                grading = 'F';
+        }
+        System.out.print("\t"+grading);
+    }
+
+    public int getAverage(){
+        return ((english+math)/2);
+    }
 }
+
