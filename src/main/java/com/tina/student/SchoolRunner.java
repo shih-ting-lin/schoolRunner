e41496f50bfa0681d4200a28ef8c951375cbfc22
shih-ting-lin <107251983+shih-ting-lin@users.noreply.github.com>
2022-09-15 16:41:52 +0800

    Update SchoolRunner.java

--- src/main/java/com/tina/student/SchoolRunner.java
+++ src/main/java/com/tina/student/SchoolRunner.java
@@ -1,2 +1,11 @@
-package com.tina.student;public class SchoolRunner {
+package com.tina.student;
+
+public class SchoolRunner {
+    public static void main(String[] args) {
+        //改Student.pass就可以直接改pass
+        Student.pass = 20;
+        Student s = new Student("1","Tommy",35,73);
+        s.print();
+    }
+
 }
