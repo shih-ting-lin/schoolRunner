60ce238a1a9ae43f3186efe454cdac3e85e80eab
shih-ting-lin <107251983+shih-ting-lin@users.noreply.github.com>
2022-09-15 16:41:21 +0800

    Update GraduateStudent.java

--- src/main/java/com/tina/student/GraduateStudent.java
+++ src/main/java/com/tina/student/GraduateStudent.java
@@ -1,2 +1,13 @@
-package com.tina.student;public class GraduateStudent {
+package com.tina.student;
+
+public class GraduateStudent extends Student {
+    int thesis;
+    static int pass = 70;
+
+    public GraduateStudent(String id, String name, int english, int math, int thesis){
+        super(id, name, english, math);
+        this.thesis = thesis;
+    }
+
+
 }
